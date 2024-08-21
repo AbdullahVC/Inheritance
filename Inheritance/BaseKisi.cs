3c501d3af43f0795179daed04be42e3a5d051cf0
AbdullahVC <69648705+AbdullahVC@users.noreply.github.com>
2024-08-21 15:37:32 +0300

    Proje dosyası ekle.

---
+++ Inheritance/BaseKisi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void KisiBilgisiYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    class Ogrenci : BaseKisi
+    {
+        public int OgrenciNumarasi { get; set; }
+
+        public void OgrenciBilgisiYazdir()
+        {
+            KisiBilgisiYazdir();
+            Console.WriteLine($"Öğrenci Numarası: {OgrenciNumarasi}");
+        }
+    }
+
+    class Ogretmen : BaseKisi
+    {
+        public decimal Maas { get; set; }
+
+        public void OgretmenBilgisiYazdir()
+        {
+            KisiBilgisiYazdir();
+            Console.WriteLine($"Maaş: {Maas} TL");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.Ad = "Ahmet";
+            ogrenci.Soyad = "Yılmaz";
+            ogrenci.OgrenciNumarasi = 12345;
+            ogrenci.OgrenciBilgisiYazdir();
+
+            Console.WriteLine("--------------------------------------------------");
+
+            Ogretmen ogretmen = new Ogretmen();
+            ogretmen.Ad = "Ayşe";
+            ogretmen.Soyad = "Kaya";
+            ogretmen.Maas = 5000.00m;
+            ogretmen.OgretmenBilgisiYazdir();
+        }
+    }
+
+}
